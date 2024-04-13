72c0178598ea0e82043b522f89ec85fafa5a42cf
Mahmoud Ali <eng.mahmoudali94@gmail.com>
2024-04-13 03:47:35 +0200

    update all

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const value of array) {
-      const idx = array.indexOf(value);
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const employees = [];
-    for (const item of reportWithIterator) {
-      employees.push(item);
-    }
-    return employees.join(' | ');
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
+
+  return employees.join(' | ');
+}
